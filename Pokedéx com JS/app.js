3b145379419194f8c806cf0be5fb44e7908a8872
Pauloricardo2019 <49963863+Pauloricardo2019@users.noreply.github.com>
2021-11-25 18:52:18 -0300

    Add files via upload

---
+++ Pokedéx com JS/app.js
@@ -0,0 +1,36 @@
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+
+const generatePokemonPromises = () => Array(898).fill().map((_,index) => 
+    fetch(getPokemonUrl(index + 1)).then(response => response.json()))
+
+const generateHTML = pokemons => pokemons.reduce((accumulator, pokemon) => {
+    const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
+    accumulator +=
+        `<li class="card ${types[0]}">
+                <img class="card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
+                <h2 class="card-tittle">${pokemon.id}. ${pokemon.name}  </h2> 
+                <p clas="card-subtitle">${types.join(' | ')}</p>
+            </li>`
+    return accumulator
+}, '')
+
+    
+
+    const insertPokemonsIntoPage = pokemons => {
+        const ul = document.querySelector('[data-js = "pokedex"]')
+
+        ul.innerHTML = pokemons;
+
+    }
+
+
+const pokemonPromises = generatePokemonPromises()
+
+Promise.all(pokemonPromises)
+    .then(generateHTML)
+    .then(insertPokemonsIntoPage)
+
+
+fetchPokemon()
